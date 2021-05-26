d25c870d1e5314a1ddaeb65adf251cf3cc8f24fc
Tru5tNo1 <delaurentis@live.it>
2021-05-26 16:52:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 # install stuff we need
 apt-get update -y
 apt-get install -y python3 python3-pip python3-openssl gcc-mingw-w64 nasm upx binutils wget apt-transport-https procps mono-complete less metasploit-framework
+
+wget http://ftp.us.debian.org/debian/pool/main/i/icu/libicu57_57.1-6+deb9u4_amd64.deb
+sudo dpkg -i libicu57_57.1-6+deb9u4_amd64.deb
+sudo apt install dotnet-sdk-3.1
+
 python3 -m pip install -r /root/shad0w/requirements.txt
 
 # install dotnet
